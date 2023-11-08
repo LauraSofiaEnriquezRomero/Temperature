af8d5cce2f24c00a07ba02183466a5cd15e0196d
YinelaPrimero <yinelaprimero01@gmail.com>
2023-11-07 23:14:32 -0500

    Evento con los años y lineas

--- Lineas/script.js
+++ Lineas/script.js
@@ -1,7 +1,27 @@
 const circlesContainer = document.getElementById("circles-container");
+const yearSelect = document.getElementById("year-select");
+const circles = [];
+
+// Función para mostrar u ocultar círculos y botones en función de la selección
+function updateCirclesAndButtons() {
+  const selectedYear = parseInt(yearSelect.value, 10);
+
+  // Ocultar todos los círculos y botones
+  circles.forEach((circle) => {
+    circle.style.display = "none";
+  });
+
+  // Mostrar los círculos y botones seleccionados
+  for (let i = 0; i < selectedYear; i++) {
+    circles[i].style.display = "block";
+  }
+}
+
+// Agregar un evento de cambio al elemento select
+yearSelect.addEventListener("change", updateCirclesAndButtons);
 
 // Crear varios círculos con botones
-const numCircles = 5; // Número de círculos a crear
+const numCircles = 10; // Número de círculos a crear
 const baseRadius = 100; // Radio inicial
 
 for (let i = 0; i < numCircles; i++) {
@@ -11,6 +31,7 @@ for (let i = 0; i < numCircles; i++) {
   const radius = baseRadius + i * 20; // Aumenta el radio para cada círculo
   circle.style.width = `${2 * radius}px`; // Aumenta el tamaño del círculo
   circle.style.height = `${2 * radius}px`; // Aumenta el tamaño del círculo
+  circles.push(circle); // Agregar el círculo a la lista de círculos
 
   const buttonsContainer = document.createElement("div");
   buttonsContainer.classList.add("buttons-container");
@@ -24,7 +45,7 @@ for (let i = 0; i < numCircles; i++) {
     button.classList.add("button");
     button.textContent = j + 1;
 
-    const angle = (j * (180 /11)) * (Math.PI / 180); // Distribuye los botones en la mitad superior del círculo
+    const angle = (j * (180 / 11)) * (Math.PI / 180); // Distribuye los botones en la mitad superior del círculo
     const x = radius * Math.cos(angle);
     const y = radius * Math.sin(angle);
 
@@ -40,7 +61,9 @@ for (let i = 0; i < numCircles; i++) {
 }
 
 // Aplicar animación a los círculos
-const circles = document.querySelectorAll(".circle");
 circles.forEach((circle, index) => {
   circle.style.animation = `appear 1s ease forwards ${index * 0.5}s`;
 });
+
+// Llamar a la función inicialmente para mostrar los círculos iniciales
+updateCirclesAndButtons();
