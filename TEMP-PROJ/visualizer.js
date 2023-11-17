b63740d835064594fd431ad0cbda887de7819740
Arthurx7 <128187709+Arthurx7@users.noreply.github.com>
2023-11-17 09:27:28 -0500

    ya sirve

--- TEMP-PROJ/visualizer.js
+++ TEMP-PROJ/visualizer.js
@@ -2,51 +2,55 @@ const btn = document.querySelector('.btn');
 const audio = document.querySelector('audio');
 const visualizer = document.querySelector('.visualizer');
 
+let ctx; // Declare the AudioContext outside the click event handler
+
 btn.addEventListener('click', e => {
+    if (!ctx) {
+        // Create AudioContext on the first user interaction
+        ctx = new (window.AudioContext || window.webkitAudioContext)();
+        const analyser = ctx.createAnalyser();
+        const source = ctx.createMediaElementSource(audio);
+        source.connect(analyser);
+        source.connect(ctx.destination);
+        analyser.fftSize = 64;
+        const bufferLength = analyser.frequencyBinCount;
+
+        let dataArray = new Uint8Array(bufferLength);
+        let elements = [];
+        for (let i = 0; i < bufferLength; i++) {
+            const element = document.createElement('span');
+            element.classList.add('element');
+            elements.push(element);
+            visualizer.appendChild(element);
+        }
+
+        const clamp = (num, min, max) => {
+            if (num >= max) return max;
+            if (num <= min) return min;
+            return num;
+        }
+
+        const update = () => {
+            requestAnimationFrame(update);
+            analyser.getByteFrequencyData(dataArray);
+            for (let i = 0; i < bufferLength; i++) {
+                let item = dataArray[i];
+                item = item > 150 ? item / 1.2 : item * 1.2;
+                elements[i].style.transform = `rotateZ(${i * (360 / bufferLength)}deg) translate(-50%, ${clamp(item, 100, 190)}px)`;
+            }
+        };
+        update();
+    }
+
+    // Start or pause audio playback
     audio.paused ? audio.play() : audio.pause();
     btn.classList.toggle('btn-play');
     btn.classList.toggle('btn-pause');
 });
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-const ctx = new window.AudioContext();
-const analyser = ctx.createAnalyser();
-const source = ctx.createMediaElementSource(audio);
-source.connect(analyser);
-source.connect(ctx.destination);
-analyser.fftSize = 64;
-const bufferLength = analyser.frequencyBinCount;
-
-let dataArray = new Uint8Array(bufferLength);
-let elements = [];
-for(let i = 0; i < bufferLength; i++) {
-    const element = document.createElement('span');
-    element.classList.add('element');
-    elements.push(element);
-    visualizer.appendChild(element);
-}
-
-const clamp = (num, min, max) => {
-    if(num >= max) return max;
-    if(num <= min) return min;
-    return num;
-}
-
-const update = () => {
-    requestAnimationFrame(update);
-    analyser.getByteFrequencyData(dataArray);
-    for (let i = 0; i < bufferLength; i++) {
-        let item = dataArray[i];
-        item = item > 150 ? item / 1.2 : item * 1.2;
-        elements[i].style.transform = `rotateZ(${i * (360 / bufferLength)}deg) translate(-50%, ${clamp(item, 100, 190)}px)`;
-    }
-};
-update();
-
-
-//SLIDER DEL VOLUMEN
+// SLIDER DEL VOLUMEN
 const volumeSlider = document.getElementById('volumeSlider');
-audio.volume = volumeSlider.value; // Establece el volumen inicial
+audio.volume = volumeSlider.value; // Set the initial volume
 
 volumeSlider.addEventListener('input', function () {
     audio.volume = volumeSlider.value;
